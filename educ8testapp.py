8e1a536731c4aafcbaf4af91fe073da1a639b571
Jord411 <64355360+Jord411@users.noreply.github.com>
2020-04-28 12:02:03 +0100

    Uncapitalised flask-sqlalchemy

--- educ8testapp.py
+++ educ8testapp.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
-from Flask_SQLAlchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
